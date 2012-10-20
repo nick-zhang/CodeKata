3ffb4cef96e1103ce9171afef1af06a30ed78f2c
Zhang Yue <zhangyue02135@yahoo.com.cn>
2012-10-20 08:47:52 +0800

    Created failure test for calculating bulls.

--- BullsAndCows/BullsAndCowsTest/UnitTest1.cs
+++ BullsAndCows/BullsAndCowsTest/UnitTest1.cs
@@ -6,8 +6,26 @@ namespace BullsAndCowsTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldReturnCorrectNumberOfBulls()
         {
+            const int numberGiven = 1234;
+            const int actualNumber = 1567;
+            var bullsAndCowsCalaulator = new BullsAndCowsCalaulator(actualNumber);
+            var bulls = bullsAndCowsCalaulator.CalculateBulls(numberGiven);
+            Assert.AreEqual(1, bulls);
+        }
+    }
+
+    public class BullsAndCowsCalaulator
+    {
+        public BullsAndCowsCalaulator(int actualNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int CalculateBulls(int numberGiven)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
